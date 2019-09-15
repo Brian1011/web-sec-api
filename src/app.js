e6cbe757d01532ef3beaedf505dec69cc069b138
MarkNjunge <mark.kamau@outlook.com>
2019-09-15 18:33:40 +0300

    Add Heroku support

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 const router = require("./router");
 const db = require("./db");
 
-const port = config.get("port") || 3000;
+const port = process.argv[2] || config.get("port") || 3000;
 
 const app = express();
 app.use(cors());
